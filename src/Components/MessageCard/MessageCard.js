3c66e339030fc47887ad11c95d15c53146db8003
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2022-08-23 02:35:07 +0300

    i create MessageCard

--- src/Components/MessageCard/MessageCard.js
+++ src/Components/MessageCard/MessageCard.js
@@ -1,10 +1,27 @@
-import {View, Text} from 'react-native';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
 import React from 'react';
-
+import styles from './MessageCard.style';
+import {useNavigation} from '@react-navigation/native';
 const MessageCard = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>MessageCard</Text>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.navigate('Chat')}>
+        {/* Tıklanılan mesajlaşmaya gidilebilinecek. */}
+        <View style={styles.imageContainer}>
+          {/* imageContainer profil fotoğrafı ve textleri yan yana almak için oluşturuldu. */}
+          <Image
+            style={styles.profilPhoto}
+            source={{
+              uri: 'https://www.erenyapidekorasyon.com.tr/wp-content/uploads/2018/11/D-10.jpg',
+            }}
+          />
+          <View>
+            <Text style={styles.firstName}>First Name</Text>
+            <Text style={styles.message}>Lorem İpsum Dolar Set Amoouu...</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -2,10 +2,12 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import styles from './Home.style';
 import Header from '../../Components/Header/Header';
+import MessageCard from '../../Components/MessageCard/MessageCard';
 const Home = () => {
   return (
     <View style={styles.container}>
       <Header />
+      <MessageCard />
     </View>
   );
 };
